9d591e18dd094fbf5645d9d53932c2c74c90059f
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-04 16:20:19 +0900

    First commit

---
+++ src/main/scala-2.11/Main.scala
@@ -0,0 +1,7 @@
+object Main {
+  val v1: Option[Int] = Some(2)
+  val v2: Option[Int] = Some(3)
+  val v3: Option[Int] = Some(5)
+  val v4: Option[Int] = Some(7)
+  val v5: Option[Int] = Some(11)
+}
