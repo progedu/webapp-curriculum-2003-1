76f7fbb7304e8423f9c94f1b3a109404a35e8083
Keito-Mac <keito19981129@gmail.com>
2022-04-14 23:19:34 +0900

    演習の解答を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,4 +4,11 @@ object Main {
   val v3: Option[Int] = Some(5)
   val v4: Option[Int] = Some(7)
   val v5: Option[Int] = Some(11)
+
+  val result = for {
+    i1 <- v1
+    i2 <- v2
+    i3 <- v3
+    i4 <- v4
+    i5 <- v5} yield i1 * i2 * i3 * i4 * i5
 }
